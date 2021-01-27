8f68ea79efa7786d176ad826aed6b3b4456c7cc8
shyam <radheshyam.srinivasan@gmail.com>
2021-01-27 23:14:44 +0800

    reformat code

--- app/src/main/java/com/shyam/currencyconverter/core/DataSourceProviderImpl.kt
+++ app/src/main/java/com/shyam/currencyconverter/core/DataSourceProviderImpl.kt
@@ -5,14 +5,16 @@ import com.shyam.currencyconverter.data.source.local.CurrencyLocalDataSource
 import com.shyam.currencyconverter.data.source.remote.CurrencyRemoteDataSource
 import com.shyam.currencyconverter.data.source.remote.network.RetrofitClient
 
-class DataSourceProviderImpl :DataSourceProvider{
+class DataSourceProviderImpl : DataSourceProvider {
     override fun provideLocalDataSource(): CurrencyLocalDataSource =
-         CurrencyLocalDataSource(
-            CurrencyConverterApplication.getDatabase())
+        CurrencyLocalDataSource(
+            CurrencyConverterApplication.getDatabase()
+        )
 
     override fun provideRemoteDataSource(): CurrencyRemoteDataSource =
         CurrencyRemoteDataSource(
-            RetrofitClient.CURRENCY_LAYER_API_INTERFACE)
+            RetrofitClient.CURRENCY_LAYER_API_INTERFACE
+        )
 
 
 }
